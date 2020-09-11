bc28b64092c34fe1079d6ac5d936c5cc94aa379d
SodaHuang <40224445+SodaHuang@users.noreply.github.com>
2020-09-11 11:05:24 +0800

    Update Calculate.java

--- Calculate.java
+++ Calculate.java
@@ -5,18 +5,15 @@ public class Calculate {
         if (null == arr || arr.length < 2) {
             solution = "please enter more than two number";
         } else {
-            Boolean isBreak = false;
+            calculate:
             for (int i = 0; i < arr.length; i++) {
                 for (int j = i + 1; j < arr.length; j++) {
                     Integer total = arr[i] + arr[j];
                     if (total.equals(sum)) {
                         solution = "{" + arr[i] + "," + arr[j] + "}";
-                        isBreak = true;
-                        break;
+                      
+                        break calculate;
                     }
-                }
-                if (isBreak) {
-                    break;
                 }
             }
         }
